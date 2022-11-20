7431f24b239d92bf6c1e2e194251d59ee47dc893
Rajnish <b421037@iiit-bh.ac.in>
2022-11-20 23:20:26 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForms from "./components/TextForms";
+import React, { userState } from "react";
 
 function App() {
-  const [darkmode, setDarkmode] = useState(false);
+  const [mode, setMode] = useState("light");
   return (
     <>
       {/* <Navbar title="textUtils" aboutText="About" /> */}
-      <Navbar title="textUtils" mode={darkmode} />
+      <Navbar title="textUtils" mode={mode} />
       <div className="container my-3">
         <TextForms heading="Enter the text to analyze below" />
         {/* <About /> */}
