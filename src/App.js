39b90bba16ddde3d8b4e02cc553b88bbdf19c7ee
Rajnish <b421037@iiit-bh.ac.in>
2023-07-31 07:03:07 +0530

    utils created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForms from "./components/TextForms";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#24243e";
+      showAlert("Dark mode enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "#E9E4F0";
+      showAlert("Light mode enabled", "success");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="textUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <Routes>
+          <Route exact path="/about" element={<About mode={mode} />} />
+          <Route
+            exact
+            path="/home"
+            element={
+              <TextForms
+                showAlert={showAlert}
+                heading="Write your text here!"
+                mode={mode}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </>
   );
 }
 
